6499508f460ce70b794100b3f5c3cb3a8d983ac3
TheUltimateNuke <nuke.public@gmail.com>
2024-12-04 21:45:39 +0000

    feat: :tada: Initial commit
    Completed GBMTemplates.Standalone

---
+++ GBMTemplates.Standalone/Mod.cs
@@ -0,0 +1,15 @@
+﻿using GBMTemplates.Standalone;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(Mod), "ModName", "AuthorName", "0.1.0")]
+[assembly: MelonGame("Boneloaf", "Gang Beasts")]
+
+namespace GBMTemplates.Standalone;
+
+public class Mod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        LoggerInstance.Msg(System.ConsoleColor.Green, $"Mod ModName initialized!");
+    }
+}
