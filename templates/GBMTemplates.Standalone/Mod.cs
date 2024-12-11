4b535dc1e057b14b4abc1d030b5cd93bef12a10c
TheUltimateNuke <nuke.public@gmail.com>
2024-12-11 18:44:24 +0000

    refactor: :memo: Add comments and Logger static accessor

--- templates/GBMTemplates.Standalone/Mod.cs
+++ templates/GBMTemplates.Standalone/Mod.cs
@@ -1,15 +1,19 @@
 ﻿using GBMTemplates.Standalone;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(Mod), "ModName", "AuthorName", "0.1.0")]
-[assembly: MelonGame("Boneloaf", "Gang Beasts")]
+[assembly: MelonInfo(typeof(Mod), "ModName" /* Mod Name */, "AuthorName" /* Your Name */, "0.1.0" /* Version */)] // Can optionally specify a download link for CementGB auto updates
+[assembly: MelonGame("Boneloaf", "Gang Beasts")] // Makes sure this mod is only loaded for Gang Beasts. Remove this if you want to use this template for other games.
 
 namespace GBMTemplates.Standalone;
 
 public class Mod : MelonMod
 {
+    public static MelonLogger.Instance Logger => Melon<Mod>.Logger; // Forwards MelonLoader's Logger to static access, so it is easier to call for it from other classes (instead of Melon<Mod>.Msg("blah blah") it is simply Mod.Logger.Msg("blah blah"))
+
     public override void OnInitializeMelon()
     {
-        LoggerInstance.Msg(System.ConsoleColor.Green, $"Mod ModName initialized!");
+        // Put starting code here
+
+        LoggerInstance.Msg(System.ConsoleColor.Green, $"Mod ModName initialized!"); // You must use MelonLoader's Logger instance, NOT Unity's Debug.Log or the system console.
     }
 }
