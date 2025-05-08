9c775a81fd3342bf0734b50107dc27300a0af874
TheUltimateNuke <nuke.public@gmail.com>
2025-05-08 13:00:39 +0000

    Generate mod info from MSBuild props

--- templates/GBMTemplates.Standalone/Mod.cs
+++ templates/GBMTemplates.Standalone/Mod.cs
@@ -1,27 +1,20 @@
 ﻿using GBMTemplates.Standalone;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(Mod), Mod.BuildInfo.Name /* Mod Name */, Mod.BuildInfo.Version /* Version (Semantic versioning convention recommended) */, Mod.BuildInfo.Author /* Your Name */, Mod.BuildInfo.DownloadLink /* Download Link for Cement auto-updating (optional, null means disabled) */)]
+// Mod info can be changed in GBMTemplates.Standalone.csproj.
+[assembly: MelonInfo(typeof(Mod), MyModInfo.Name /* Mod Name */, MyModInfo.Version /* Version (Semantic versioning convention recommended) */, MyModInfo.Author /* Your Name */, null /* Download Link (optional, null means disabled) */)]
 [assembly: MelonGame("Boneloaf", "Gang Beasts")] // Makes sure this mod is only loaded for Gang Beasts. Remove this if you want to use this template for other games.
 
 namespace GBMTemplates.Standalone;
 
 public class Mod : MelonMod
 {
-    public static class BuildInfo
-    {
-        public const string Name = "ModName";
-        public const string Author = "AuthorName";
-        public const string Version = "0.1.0";
-        public const string DownloadLink = null;
-    }
-
     public static MelonLogger.Instance Logger => Melon<Mod>.Logger; // Forwards MelonLoader's Logger to static access, so it is easier to call for it from other classes (instead of Melon<Mod>.Msg("blah blah") it is simply Mod.Logger.Msg("blah blah"))
 
     public override void OnInitializeMelon()
     {
         // Put starting code here
 
-        LoggerInstance.Msg(System.ConsoleColor.Green, $"Mod ModName initialized!"); // You must use MelonLoader's Logger instance, NOT Unity's Debug.Log or the system console.
+        LoggerInstance.Msg(System.ConsoleColor.Green, $"Mod {MyModInfo.Name} initialized!"); // You must use MelonLoader's Logger instance, NOT Unity's Debug.Log or the system console.
     }
 }
